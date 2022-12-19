76d3160006e0c6be52688fb19194c90c264fd6c9
jeancharles <jeancduasan@gmail.com>
2022-12-19 14:21:17 -0300

    H2 Database Created

---
+++ backend/src/main/java/com/jean/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.jean.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jean.dscatalog.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+	
+
+}
---
+++ backend/src/main/java/com/jean/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.jean.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jean.dscatalog.entities.Category;
+import com.jean.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}
